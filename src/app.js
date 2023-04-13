65e93b99b0fb8863a8aa53325f5175d5b35bf334
samriddhipuranik <69351209+samriddhipuranik@users.noreply.github.com>
2023-04-13 11:41:32 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,31 +1,18 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { off } = require('../models/user.js');
-const users   =require("../models/user.js");
+const User = require('../models/user.js');
 
-
-// Import routes
-
-//Router Middlewares
 app.use(express.json());
 
-
-//default value for limit is 5 and offset is 0
-//This route should return an array of _id of all the element that need to be returned.
-//output id can be in any order.
-
-app.get("/",async function(req,res){
-
-    var ids = [];
-
-    //Write your code here
-    //modify the ids array
-
-    res.send(ids);
-
+app.get("/", async function(req, res) {
+  let limit = parseInt(req.query.limit) || 5;
+  let offset = parseInt(req.query.offset) || 0;
+  let users = await User.find({}, '_id')
+                        .skip(limit * offset)
+                        .limit(limit);
+  let ids = users.map(user => user._id);
+  res.send(ids);
 });
 
 module.exports = app;
-
-
